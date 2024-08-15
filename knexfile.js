3447cee1fb8d1a09c628cb36d18e88e6f4330770
Kenichi Ishibashi <Kenichishibashi@gmail.com>
2024-08-14 17:32:15 -0700

    changed the url for the elephant database on knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -3,14 +3,14 @@ const path = require("path");
 require("dotenv").config();
 
 const {
-  DATABASE_URL = "postgres://dguqwbbs:tMxEew0sTOK1o2eWfCOhSmvpeZJf5ESr@flora.db.elephantsql.com/dguqwbbs?ssl=true",
+  DATABASE_URL = "postgres://dguqwbbs:H820V-SJKRyWajCaJ0IlTHM8vDO92tbR@flora.db.elephantsql.com/dguqwbbs",
 } = process.env;
 
 module.exports = {
   development: {
     client: "postgresql",
     connection: DATABASE_URL,
-    pool: { min: 0, max: 5 },
+    pool: { min: 2, max: 10 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
